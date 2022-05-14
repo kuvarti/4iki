29684761f23f88077802190e2756c56ca9655660
kuvarti <uzali.kafasi@gmail.com>
2022-05-14 07:49:13 +0300

    smart contract string operation

--- smart_contract.c
+++ smart_contract.c
@@ -1,7 +1,34 @@
 #include "signal.h"
 #include "stdio.h"
+#include "string.h"
 
 unsigned char	g_gelen = 0;
+char	*msaj;
+
+char	*stradd(char *s1, char s2)
+{
+	unsigned int	i;
+	unsigned int	j;
+	char			*s3;
+
+	if (!s1)
+	{
+		s1 = malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	s3 = malloc(ft_strlen(s1) + 2);
+	if (!s3)
+		return (NULL);
+	i = -1;
+	while (s1[++i] != 0)
+		s3[i] = s1[i];
+	s3[i++] = s2;
+	s3[i] = 0;
+	free(s1);
+	return (s3);
+}
 
 void	birsinyal(int sig, siginfo_t *si, void *undelete)
 {
@@ -15,10 +42,8 @@ void	birsinyal(int sig, siginfo_t *si, void *undelete)
 	bit++;
 	if (bit == 8)
 	{
-		printf("%c", g_gelen);
 		bit = 0;
-		kill(si->si_pid, SIGUSR1);
-		undelete = "error verme";
+		msaj = stradd(msaj, g_gelen);
 	}
 }
 
